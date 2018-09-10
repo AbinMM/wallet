fa06dcf4f1d6e42ab6aba6d4f3fd4f75f8a9b9f4
marco <lxgaisqq@163.com>
2018-09-10 20:34:09 +0800

    我的交易，如未导入账号情况，定时刷新时去掉提示语

--- app/route/Transaction/index.js
+++ app/route/Transaction/index.js
@@ -358,7 +358,9 @@ class Transaction extends BaseComponent {
         this.setState({selectedTransactionRecord:opt});
         if(type == 0){
             if (this.props.defaultWallet == null || this.props.defaultWallet.account == null || !this.props.defaultWallet.isactived || !this.props.defaultWallet.hasOwnProperty('isactived')) {
-                EasyToast.show('请导入您已激活的账号');
+                if(!onRefreshing){
+                    EasyToast.show('请导入您已激活的账号');
+                }
             }else{
                 if(!onRefreshing){
                     this.setState({logRefreshing: true});
