08dece149387fbe439420a03264d17688fe65e27
jason <273866181@qq.com>
2018-09-11 10:53:45 +0800

    Fixed #493 EosToken APP图标、启动、欢迎页替换成ET

--- app/route/Boot.js
+++ app/route/Boot.js
@@ -30,9 +30,9 @@ class Boot extends React.Component {
                 <Swiper loop={false} activeDotColor={UColor.tintColor}>
                     <View style={{justifyContent:'center',alignItems:'center',height:'100%'}}>
                         <View style={{height:'30%',justifyContent:'center',alignItems:'center'}}>
-                            <Text style={{color:UColor.btnColor,fontSize:24,textAlign:'center'}}>ET去中心化交易所</Text>
-                            <Text style={{color:UColor.btnColor,fontSize:18,marginTop:30,textAlign:'center'}}>——链上治理，公开透明</Text>
-                            {/* <Text style={{color:UColor.fontColor,fontSize:18,textAlign:'center',marginTop:10}}>去中心化钱包，无第三方留存</Text> */}
+                            <Text style={{color:UColor.btnColor,fontSize:24,textAlign:'center',fontWeight: "bold"}}>ET去中心化交易所</Text>
+                            <Text style={{color:UColor.mainsecd,fontSize:18,marginTop:30,textAlign:'center'}}>——链上治理，公开透明</Text>
+                            {/* <Text style={{color:UColor.mainsecd,fontSize:18,textAlign:'center',marginTop:10}}>去中心化钱包，无第三方留存</Text> */}
                         </View>
                         <View style={{height:'50%',alignItems:'center'}}>
                             <Image source={UImage.a} style={{width:210,height:253,marginTop:50}} />
@@ -40,9 +40,9 @@ class Boot extends React.Component {
                     </View>
                     <View style={{justifyContent:'center',alignItems:'center',height:'100%'}}>
                         <View style={{height:'30%',justifyContent:'center',alignItems:'center'}}>
-                            <Text style={{color:UColor.btnColor,fontSize:24,textAlign:'center'}}>世界的另一头，近在咫尺</Text>
-                            <Text style={{color:UColor.btnColor,fontSize:18,marginTop:30,textAlign:'center'}}>快速转账，无手续费</Text>
-                            <Text style={{color:UColor.btnColor,fontSize:18,textAlign:'center',marginTop:10}}>百万级高并发无压力</Text>
+                            <Text style={{color:UColor.btnColor,fontSize:24,textAlign:'center',fontWeight: "bold"}}>世界的另一头，近在咫尺</Text>
+                            <Text style={{color:UColor.mainsecd,fontSize:18,marginTop:30,textAlign:'center'}}>快速转账，无手续费</Text>
+                            <Text style={{color:UColor.mainsecd,fontSize:18,textAlign:'center',marginTop:10}}>百万级高并发无压力</Text>
                         </View>
                         <View style={{height:'50%',alignItems:'center'}}>
                             <Image source={UImage.b} style={{width:245,height:253,marginTop:50}} />
@@ -50,9 +50,9 @@ class Boot extends React.Component {
                     </View>
                     <View style={{justifyContent:'center',alignItems:'center',height:'100%'}}>
                         <View style={{height:'30%',justifyContent:'center',alignItems:'center'}}>
-                            <Text style={{color:UColor.btnColor,fontSize:24,textAlign:'center'}}>您关心的EOS资讯</Text>
-                            <Text style={{color:UColor.btnColor,fontSize:18,marginTop:30,textAlign:'center'}}>您最关心的EOS独家情报</Text>
-                            <Text style={{color:UColor.btnColor,fontSize:18,textAlign:'center',marginTop:10}}>最新、最快、最全面</Text>
+                            <Text style={{color:UColor.btnColor,fontSize:24,textAlign:'center',fontWeight: "bold"}}>您关心的EOS资讯</Text>
+                            <Text style={{color:UColor.mainsecd,fontSize:18,marginTop:30,textAlign:'center'}}>您最关心的EOS独家情报</Text>
+                            <Text style={{color:UColor.mainsecd,fontSize:18,textAlign:'center',marginTop:10}}>最新、最快、最全面</Text>
                         </View>
                         <View style={{height:'50%',alignItems:'center'}}>
                             <Image source={UImage.c} style={{width:215,height:253,marginTop:50}} />
@@ -60,13 +60,13 @@ class Boot extends React.Component {
                     </View>
                     <View style={{justifyContent:'center',alignItems:'center',height:'100%'}}>
                         <View style={{height:'30%',justifyContent:'center',alignItems:'center'}}>
-                            <Text style={{color:UColor.btnColor,fontSize:24,textAlign:'center'}}>欢迎使用EosToken</Text>
-                            <Text style={{color:UColor.btnColor,fontSize:18,marginTop:30,textAlign:'center'}}>您的EOS数字资产管家</Text>
+                            <Text style={{color:UColor.btnColor,fontSize:24,textAlign:'center',fontWeight: "bold"}}>欢迎使用EosToken</Text>
+                            <Text style={{color:UColor.mainsecd,fontSize:18,marginTop:30,textAlign:'center'}}>您的EOS数字资产管家</Text>
                         </View>
                         <View style={{height:'50%',alignItems:'center'}}>
                             <Image source={UImage.d} style={{width:215,height:283,marginTop:50}} />
                         </View>
-                        <Text onPress={()=>{this.comin()}} style={{color:UColor.btnColor,fontSize:22,width:100,textAlign:"center",position: 'absolute',left: ScreenWidth-100,right: 0,top: ScreenHeight-80,bottom: 0,}}>进入→</Text>
+                        <Text onPress={()=>{this.comin()}} style={{color:UColor.fonttint,fontSize:22,width:100,textAlign:"center",position: 'absolute',left: ScreenWidth-100,right: 0,top: ScreenHeight-80,bottom: 0,}}>进入→</Text>
                     </View>
                 </Swiper>
             </ImageBackground>
@@ -78,7 +78,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection:'column',
-        
     }
 });  
 export default Boot;
