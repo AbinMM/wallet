be58ffcbc39d2ea71bc02d723a27b836e1e0adf9
marco <lxgaisqq@163.com>
2018-09-27 14:32:05 +0800

    issue #526,vivo等手机兼容性问题

--- app/utils/EosUtil.js
+++ app/utils/EosUtil.js
@@ -1,68 +1,83 @@
-/**
- *
- * Copyright 2016-present reading
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-import { Eos } from "react-native-eosjs";
+import '../shim.js'
+import eos from 'eosjs'
+var ecc = require('eosjs-ecc');
 
-var MD5 = require("crypto-js/md5");
-var CryptoJS = require("crypto-js");
-var RSAKey = require('react-native-rsa');
+const RPC_API_URL = "http://47.52.250.41:8001";
 
-export const EosUpdateAuth = (account, pvk, auth, callback) => { 
-  if (account == null) {
-    if(callback) callback("无效账号");
-    return;
-  };
+export class Eos {
+    static respSucc(e){
+        return {isSuccess:!0,msg:"success",data:e};
+    }
+    
+    static respErr(e){
+        return {isSuccess:!1,msg:"error",data:e};
+    }
+    
+    static getEos(){
+        config = {
+          httpEndpoint: RPC_API_URL,
+          chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+        }
+      
+        var eoss = eos(config);
+        return eoss;
+      }
+      
+      
+    static getInfo(){
+        let eos = Eos.getEos();
+        return eos.getInfo({}).then(info => {
+            return info;
+        });
+    }
 
-  Eos.transaction({
-      actions: [
-          {
-              account: "eosio",
-              name: "updateauth", 
-              authorization: [{
-              actor: account,
-              permission: 'active'
-              }], 
-              data: {
-                  account: account,
-                  permission: 'active',
-                  parent: "owner",
-                  auth: auth,
-                  // auth: {
-                  //   threshold: 1,
-                  //   keys: [
-                  //       {
-                  //           key: this.props.defaultWallet.activePublic,
-                  //           weight: 1,
-                  //       }
-                  //   ],
-                  //   accounts: [
-                  //       {
-                  //           permission: {
-                  //               actor: "etbexchanger",
-                  //               permission: "eosio.code",
-                  //           },
-                  //           weight: 1,
-                  //       }
-                  //   ],
-                  // },
-              }
-          }
-      ]
-  }, pvk, (r) => {
-    if(callback) callback(r);
-  });
-};
+    static checkPrivateKey(privatekey,callback){
+        try{
+            if(ecc.isValidPrivate(privatekey)){
+                callback(Eos.respSucc(!0));
+            }else{
+                callback(Eos.respErr({code:500,msg:"私钥格式错误"}));
+            }
+        }catch(t){
+            callback(Eos.respErr({code:500,msg:"校验私钥失败"}));
+        }
+    }
+
+    static privateToPublic(privateKey,callback){
+        try{
+            var publicKey=ecc.privateToPublic(privateKey);
+            callback(Eos.respSucc({publicKey:publicKey}));
+        }catch(t){
+            callback(Eos.respErr({code:500,msg:"生成公钥失败"}));
+        }
+    }
+
+    /**
+    * 生成随机私钥
+    * @param {回调函数} callback 
+    */
+    static randomPrivateKey(callback){
+        try{
+            ecc.randomKey().then(privateKey => {
+                alert('Private Key:\t', privateKey) // wif
+                alert('Public Key:\t', ecc.privateToPublic(privateKey)) // EOSkey...
+            })
+            // ecc.randomKey().then(t =>{
+            //     alert(t)
+            //     if(t){
+            //         var e=t;
+            //         var r=ecc.privateToPublic(e);
+            //         callback(Eos.respSucc({ownerPrivate:e,ownerPublic:r}));
+            //     }else{
+            //         callback(Eos.respErr({code:500,msg:"生成私钥失败"}));
+            //     } 
+            // }).catch(function(t){
+            //     alert("222");
+            //     callback(Eos.respErr({code:500,msg:"生成私钥失败"}));
+            // })
+        }catch(t){
+            callback(Eos.respErr({code:500,msg:"生成私钥失败"}));
+        } 
+    }
+}
+
