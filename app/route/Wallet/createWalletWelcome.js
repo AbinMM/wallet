101e73b59fd8b9a193f6396d27e22def86d264fc
Ben002 <bencgping@gmail.com>
2018-11-10 22:41:53 +0800

    去掉创建钱包加粗

--- app/route/Wallet/createWalletWelcome.js
+++ app/route/Wallet/createWalletWelcome.js
@@ -84,6 +84,44 @@ class createWalletWelcome extends BaseComponent {
       }
 
 
+
+      //已激活账号需要验证密码
+  deleteWallet (paramsdata) {
+    EasyShowLD.dialogClose();
+    AuthModal.show(paramsdata.account, (authInfo) => {
+      try {
+          if(authInfo.isOk){
+              var data = paramsdata;
+              var ownerPrivateKey = paramsdata.ownerPrivate;
+              var bytes_words = CryptoJS.AES.decrypt(ownerPrivateKey.toString(), authInfo.password + paramsdata.salt);
+              var plaintext_words = bytes_words.toString(CryptoJS.enc.Utf8);
+              if (plaintext_words.indexOf('eostoken') != - 1) {
+                plaintext_words = plaintext_words.substr(8, plaintext_words.length);
+                const { dispatch } = this.props;
+                this.props.dispatch({ type: 'wallet/delWallet', payload: { data } });
+                //删除tags
+                JPushModule.deleteTags([data.name],map => {
+                  if (map.errorCode === 0) {
+                    console.log('Delete tags succeed, tags: ' + map.tags)
+                  } else {
+                    console.log(map)
+                    console.log('Delete tags failed, error code: ' + map.errorCode)
+                  }
+                });
+                DeviceEventEmitter.addListener('delete_wallet', (tab) => {
+                  this.props.navigation.goBack();
+                });
+              } else {
+                EasyToast.show('您输入的密码不正确');
+              }
+          }
+          EasyShowLD.dialogClose();
+      } catch (error) {
+        EasyShowLD.dialogClose();
+        EasyToast.show('未知异常');
+      }
+    });
+  }
     //未激活账号直接删除
     deletionDirect (paramsdata) {
         EasyShowLD.dialogClose();
@@ -103,6 +141,49 @@ class createWalletWelcome extends BaseComponent {
         });
     }
 
+    deleteWarning(c){
+        AlertModal.show("免责声明","删除过程中会检测您的账号是否已激活，如果您没有备份私钥，删除后将无法找回！请确保该账号不再使用后再删除！",'下一步','返回钱包',(resp)=>{
+          if(resp){
+            EasyShowLD.loadingShow();
+            //检测账号是否已经激活
+            this.props.dispatch({
+                type: "wallet/isExistAccountNameAndPublicKey", payload: {account_name: c.name, owner: c.ownerPublic, active: c.activePublic}, callback:(result) =>{
+                  EasyShowLD.dialogClose();
+                  if(result.code == 0 && result.data == true){
+                    AlertModal.show("免责声明","系统检测到该账号已经激活!如果执意删除请先导出私钥并保存好，否则删除后无法找回。",'执意删除','返回钱包',(resp)=>{
+                      if(resp){
+                          this.deleteWallet(c);
+                        }
+                        EasyShowLD.dialogClose();
+                      });
+                  }else if(result.code == 521){
+                    AlertModal.show("免责声明","系统检测到该账号还没激活，如果您不打算激活此账号，建议删除。",'删除','取消',(resp)=>{
+                      if(resp){
+                        this.deletionDirect(c);
+                        }
+                        EasyShowLD.dialogClose();
+                      });
+                  }else if(result.code == 515){
+                    AlertModal.show("免责声明","系统检测到该账号已经被别人抢注，强烈建议删除。",'删除','取消',(resp)=>{
+                      if(resp){
+                        this.deletionDirect(c);
+                        }
+                        EasyShowLD.dialogClose();
+                      });
+                  }else {
+                    AlertModal.show("免责声明","网络异常, 暂不能检测到账号是否已经激活, 建议暂不删除此账号, 如果执意删除请先导出私钥并保存好，否则删除后无法找回。",'执意删除','取消',(resp)=>{
+                      if(resp){
+                        this.deletionDirect(c);
+                        }
+                        EasyShowLD.dialogClose();
+                      });
+                  }
+                }
+            })
+          }
+        });
+      }
+
     // 创建钱包
     goCreateWallet() {
         if(this.props.walletList != null){
@@ -114,7 +195,7 @@ class createWalletWelcome extends BaseComponent {
                 if(resp){
                     this.activeWalletOnServer(unActiveWallet);
                     }else{
-                    // this.deleteWarning(this.props.walletList[i]);
+                    // this.deleteWarning(unActiveWallet);
                     this.deletionDirect(unActiveWallet);
                     }
                 });
@@ -141,14 +222,14 @@ class createWalletWelcome extends BaseComponent {
         return (<View style={[styles.container,{backgroundColor: UColor.secdColor}]}>
          <Header {...this.props} onPressLeft={true} title="创建钱包" />
 
-            <Text style={{fontSize: ScreenUtil.setSpText(18),lineHeight: ScreenUtil.autoheight(25),fontWeight:"bold",paddingHorizontal:ScreenUtil.autowidth(15), paddingTop: ScreenUtil.autowidth(22),color: "#262626"}}>我是老手有钱包</Text>
+            <Text style={{fontSize: ScreenUtil.setSpText(18),lineHeight: ScreenUtil.autoheight(25),paddingHorizontal:ScreenUtil.autowidth(15), paddingTop: ScreenUtil.autowidth(22),color: "#262626"}}>我是老手有钱包</Text>
             <Button onPress={()=>this.importWallet()}>
                 <View style={{marginTop: ScreenUtil.autowidth(15),marginHorizontal:ScreenUtil.autowidth(15),height: ScreenUtil.autowidth(70),borderRadius: 8,flexDirection: 'row',backgroundColor: "#FFFFFF"}}>
                     <View style={{paddingLeft:ScreenUtil.autowidth(18),flexDirection: "row",alignItems: "center",}}>  
                         <Image source={UImage.walletIcon} style={ {width: ScreenUtil.autowidth(41),height: ScreenUtil.autowidth(35),}} />
                     </View> 
                     <View style={{paddingVertical:ScreenUtil.autowidth(16),paddingHorizontal: ScreenUtil.autowidth(24),flexDirection: 'column',justifyContent: "space-between",}}>
-                        <Text style={{fontSize: ScreenUtil.setSpText(16),lineHeight: ScreenUtil.autoheight(23),fontWeight:"bold",color: "#323232"}}>导入已有钱包</Text>
+                        <Text style={{fontSize: ScreenUtil.setSpText(16),lineHeight: ScreenUtil.autoheight(23),color: "#323232"}}>导入已有钱包</Text>
                         <Text style={{fontSize: ScreenUtil.setSpText(10),lineHeight: ScreenUtil.autoheight(14),color: "#808080"}}>通过私钥导入您的现有钱包</Text>
                     </View>
                     <View style={{paddingLeft:ScreenUtil.autowidth(80),flexDirection: "row",alignItems: "center",}}>  
@@ -158,14 +239,14 @@ class createWalletWelcome extends BaseComponent {
             </Button>
 
 
-            <Text style={{fontSize: ScreenUtil.setSpText(18),lineHeight: ScreenUtil.autoheight(25),fontWeight:"bold",paddingHorizontal:ScreenUtil.autowidth(15), paddingTop: ScreenUtil.autowidth(22),color: "#262626"}}>我是小白没钱包</Text>
+            <Text style={{fontSize: ScreenUtil.setSpText(18),lineHeight: ScreenUtil.autoheight(25),paddingHorizontal:ScreenUtil.autowidth(15), paddingTop: ScreenUtil.autowidth(22),color: "#262626"}}>我是小白没钱包</Text>
             <Button onPress={()=>this.goCreateWallet()}>
                 <View style={{marginTop: ScreenUtil.autowidth(15),marginHorizontal:ScreenUtil.autowidth(15),height: ScreenUtil.autowidth(70),borderRadius: 8,flexDirection: 'row',backgroundColor: "#FFFFFF"}}>
                     <View style={{paddingLeft:ScreenUtil.autowidth(18),flexDirection: "row",alignItems: "center",}}>  
                         <Image source={UImage.love} style={ {width: ScreenUtil.autowidth(40),height: ScreenUtil.autowidth(38),}} />
                     </View> 
                     <View style={{paddingVertical:ScreenUtil.autowidth(16),paddingLeft:ScreenUtil.autowidth(24),flexDirection: 'column',justifyContent: "space-between",}}>
-                        <Text style={{fontSize: ScreenUtil.setSpText(16),lineHeight: ScreenUtil.autoheight(23),fontWeight:"bold",color: "#323232"}}>1分钟快速创建</Text>
+                        <Text style={{fontSize: ScreenUtil.setSpText(16),lineHeight: ScreenUtil.autoheight(23),color: "#323232"}}>1分钟快速创建</Text>
                         <Text style={{fontSize: ScreenUtil.setSpText(10),lineHeight: ScreenUtil.autoheight(14),color: "#808080"}}>填写关键信息后，让好友帮忙或微信支付来创建</Text>
                     </View>
                     <View style={{paddingLeft:ScreenUtil.autowidth(10),flexDirection: "row",alignItems: "center",}}>  
