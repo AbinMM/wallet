8255bd5e531aed7d239d779d4d3e3ae6a12e2c63
Ben002 <bencgping@gmail.com>
2018-11-12 15:49:34 +0800

    备份私钥展示私钥部分代码整理

--- app/route/Wallet/BackupsPkey.js
+++ app/route/Wallet/BackupsPkey.js
@@ -32,10 +32,9 @@ class BackupsPkey extends BaseComponent {
         super(props);
         this.state = {
             password: "",
+            walletPK:"",
             ownerPk: '',
             activePk: '',
-            samePk:false,
-            show: false,
         }
     }
 
@@ -47,19 +46,12 @@ class BackupsPkey extends BaseComponent {
          var activePrivateKey = this.props.navigation.state.params.wallet.activePrivate;
          var bytes_words_active = CryptoJS.AES.decrypt(activePrivateKey.toString(), this.props.navigation.state.params.password + this.props.navigation.state.params.wallet.salt);
          var plaintext_words_active = bytes_words_active.toString(CryptoJS.enc.Utf8);
-        if (plaintext_words_owner.indexOf('eostoken') != - 1) {
-            if(plaintext_words_owner==plaintext_words_active){
-                this.setState({
-                    activePk: plaintext_words_active.substr(8, plaintext_words_active.length),
-                    samePk:true,
-                })
-            }else{
-                this.setState({
-                    ownerPk: plaintext_words_owner.substr(8, plaintext_words_owner.length),
-                    activePk: plaintext_words_active.substr(8, plaintext_words_active.length),
-                    samePk:false,
-                })
-            }
+        if ((plaintext_words_active.indexOf('eostoken') != - 1)&&(plaintext_words_owner.indexOf('eostoken') != - 1))  {
+            this.setState({
+                walletPK:plaintext_words_active.substr(8, plaintext_words_active.length),
+                ownerPk: plaintext_words_owner.substr(8, plaintext_words_owner.length),
+                activePk: plaintext_words_active.substr(8, plaintext_words_active.length),
+            })
         }
     }
 
@@ -80,34 +72,22 @@ class BackupsPkey extends BaseComponent {
         this.props.navigation.dispatch(action);
     }
 
-    toBackup = (data) => {
-        this.props.navigation.goBack();
-        const { navigate } = this.props.navigation;
-        navigate('BackupWords', data);
-    }
+    // toBackup = (data) => {
+    //     this.props.navigation.goBack();
+    //     const { navigate } = this.props.navigation;
+    //     navigate('BackupWords', data);
+    // }
     
     dismissKeyboardClick() {
       dismissKeyboard();
     }
 
-    checkClick() {
-    this.setState({
-        show: false
-    });
-    }
 
     prot(key, data = {}) {
         const { navigate } = this.props.navigation; 
-        if(key == 'activePk'){
-            Clipboard.setString(this.state.activePk);
-            if(this.state.samePk){
-                EasyToast.show('钱包私钥已复制成功');
-            }else{
-                EasyToast.show('Active私钥已复制成功');
-            }
-        } else if(key == 'ownerPk'){
-          Clipboard.setString(this.state.ownerPk);
-          EasyToast.show('Owner私钥已复制成功');
+        if(key == 'walletPK'){
+            Clipboard.setString(this.state.walletPK);
+            EasyToast.show('钱包私钥已复制成功');
         }else  if(key == 'problem') {
           navigate('Web', { title: "什么是私钥", url: "http://news.eostoken.im/html/Keystore.html" });   
         }
@@ -124,9 +104,8 @@ class BackupsPkey extends BaseComponent {
     render() {
         return (<View style={[styles.container,{backgroundColor: UColor.secdColor}]}>
          <Header {...this.props} onPressLeft={true} title="备份私钥" />
-        <TouchableOpacity activeOpacity={1.0} onPress={this.dismissKeyboardClick.bind(this)} style={styles.scrollView}>
+        <TouchableOpacity activeOpacity={1.0} onPress={this.dismissKeyboardClick.bind(this)} style={{flex: 1,}}>
             <View style={styles.header}>
-            {/* <View style={{flexDirection: 'row'}}> */}
             <View style={{paddingTop: ScreenUtil.autowidth(20), flexDirection: 'row', alignItems: 'center',justifyContent: 'center',} }>
                 <Text style={{fontSize: ScreenUtil.setSpText(18),lineHeight: ScreenUtil.autoheight(25),fontWeight:"bold", 
                 color: "#262626"}}>抄写下您的钱包私钥</Text>
@@ -136,9 +115,9 @@ class BackupsPkey extends BaseComponent {
             paddingTop: ScreenUtil.autowidth(15),color: "#808080"}}>私钥用于回复钱包或充值钱包密码，请将它准确抄写在之上，并存放在只有你知道的安全的地方。</Text>
 
             <View style={{paddingTop: ScreenUtil.autowidth(20), flexDirection: 'row',alignContent: 'center',justifyContent: 'center',} }>
-                <TouchableHighlight style={{flexDirection: 'row',alignContent: 'center',justifyContent: 'center',}} onPress={this.prot.bind(this, 'activePk')}>
+                <TouchableHighlight style={{flexDirection: 'row',alignContent: 'center',justifyContent: 'center',}} onPress={this.prot.bind(this, 'walletPK')}>
                     <Text style={{fontSize: ScreenUtil.setSpText(18),lineHeight: ScreenUtil.autoheight(25),paddingHorizontal:ScreenUtil.autowidth(16), textAlign: 'center',
-                    color: "#323232",backgroundColor: UColor.mainColor}}>{""+this.state.activePk.replace(/(.{4})/g,'$1 ')}</Text>
+                    color: "#323232",backgroundColor: UColor.mainColor}}>{""+this.state.walletPK.replace(/(.{4})/g,'$1 ')}</Text>
                 </TouchableHighlight>
             </View>
 
@@ -156,25 +135,12 @@ class BackupsPkey extends BaseComponent {
 }
 
 const styles = StyleSheet.create({
-    passoutsource: {
-        flexDirection: 'column', 
-        alignItems: 'center'
-    },
-    inptpass: {
-        textAlign: "center",
-        borderBottomWidth: 1,
-        width: ScreenWidth-100,
-        height: ScreenUtil.autoheight(45),
-        fontSize: ScreenUtil.setSpText(16),
-        paddingBottom: ScreenUtil.autoheight(5),
-    },
+
     container: {
         flex: 1,
         flexDirection: 'column',
     },
-    scrollView: {
-        flex: 1,
-    },
+
     header: {
         flex: 1,
         justifyContent: 'center',
@@ -184,82 +150,6 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         backgroundColor: UColor.mainColor,
         paddingBottom:ScreenUtil.autowidth(55),
-    },
-    inptoutbg: {
-        marginBottom: ScreenUtil.autoheight(10),
-        paddingHorizontal: ScreenUtil.autowidth(20),
-    },
-    headout: {
-        paddingTop: ScreenUtil.autoheight(20),
-        paddingBottom: ScreenUtil.autoheight(15),
-    },
-    inptitle: {
-        fontSize: ScreenUtil.setSpText(15),
-        lineHeight: ScreenUtil.autoheight(30),
-    },
-    warningout: {
-        borderWidth: 1,
-        borderRadius: 5,
-        width: ScreenWidth-40,
-        flexDirection: "column",
-        alignItems: 'center', 
-        paddingVertical: ScreenUtil.autoheight(5),
-        paddingHorizontal: ScreenUtil.autowidth(10),
-    },
-    significanttextHead: {
-        fontWeight:"bold",
-        fontSize: ScreenUtil.setSpText(16), 
-    },
-    imgBtn: {
-        width: ScreenUtil.autowidth(20),
-        height: ScreenUtil.autowidth(20),
-        marginRight: ScreenUtil.autowidth(10),
-    },
-    headtitle: {
-        fontSize: ScreenUtil.setSpText(14),
-        lineHeight: ScreenUtil.autoheight(25),
-    },
-    inptoutgo: {
-        paddingBottom: ScreenUtil.autoheight(15),
-    },
-    inptgo: {
-        height: ScreenUtil.autoheight(60),
-        paddingHorizontal: ScreenUtil.autowidth(10),
-        width: ScreenWidth - ScreenUtil.autowidth(40),
-    },
-    inptext: {
-        flexWrap: 'wrap',
-        fontSize: ScreenUtil.setSpText(14),
-        lineHeight: ScreenUtil.autoheight(25),
-    },
-    readtext: {
-        textAlign: 'right',
-        fontSize: ScreenUtil.setSpText(15),
-    },
-    importPriout: {
-        borderRadius: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: ScreenUtil.autoheight(45),
-        marginTop: ScreenUtil.autoheight(30),
-        marginHorizontal: ScreenUtil.autowidth(20),
-    },
-    importPritext: {
-        fontSize: ScreenUtil.setSpText(15),
-    },
-    logout:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        paddingBottom: ScreenUtil.autoheight(20),
-    },
-    logimg: {
-        width: ScreenUtil.autowidth(50), 
-        height: ScreenUtil.autowidth(50),
-    },
-    logtext: {
-        fontSize: ScreenUtil.setSpText(14),
-        lineHeight: ScreenUtil.autoheight(30),
     },
 });
 
